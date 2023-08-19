6fcc79f4b2c549d5356e263a656fd114f851bb4a
ahmedosama20001 <ahmedosam.2001@gmail.com>
2023-08-20 01:40:05 +0300

    1st term

---
+++ Unit 3_Embedded_C/Lesson 1/Toggle_LED/Toggle_LED/Src/main.c
@@ -0,0 +1,46 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Ahmed Osama
+ * @brief          : Toggle LED
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+#include <stdint.h>
+
+#define RCC_BASE        0x40021000
+#define portA_BASE      0x40010800
+
+#define RCC_APB2ENR    *(volatile uint32_t *) (RCC_BASE + 0x18)
+#define GPIOA_CRH      *(volatile uint32_t *) (portA_BASE + 0x4)
+#define GPIOA_ODR      *(volatile uint32_t *) (portA_BASE + 0x0c)
+
+
+
+
+int main(void)
+{
+	RCC_APB2ENR |= 1<<2 ;
+	GPIOA_CRH &= 0xff0fffff;
+	GPIOA_CRH |= 0x00200000;
+
+	while (1)
+	{
+		GPIOA_ODR |= 1<<13; // set bit 13
+		for(int i = 0 ;i <=5000;i++);
+		GPIOA_ODR &= ~(1<<13); // clear bit 13
+		for(int i = 0 ;i <=5000;i++);
+	}
+
+
+}
